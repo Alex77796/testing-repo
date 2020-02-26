7c35affdc849804e239ccd3bfdeaef9956d87f80
Alex777 <alexalasyuk@gmail.com>
2020-02-26 17:35:10 +0200

    psihology proj

--- main.js
+++ main.js
@@ -1,55 +1,15 @@
-let allArticles = [];
+// //type: null, undefined, boolean, string, number, symbol, object
 
-function saveArticle() {
-  let articleObj = {};
-  let title = document.getElementById("title").value;
-  let imgUrl = document.getElementById("imgsrc").value;
-  let article = document.getElementById("text").value;
-  if (title === "" || imgUrl === "" || article === "") {
-    alert("All inputs should not be empty");
-  } else {
-    articleObj.title = title;
-    articleObj.image = imgUrl;
-    articleObj.article = article;
-    articleObj.sentences = article.split(".");
-    articleObj.sentencesNum = articleObj.sentences.length;
-    allArticles.push(articleObj);
-    let info = document.getElementById("info");
-    info.innerText = "The article has bee saved";
-    emptyFields();
-    console.dir(allArticles);
+// //Приведення типів
+// // false: false, NaN, undefined, 0, null, ""
+
+menu.onclick = function myFunction(){
+    let x = document.getElementById("myTopnav");
+  
+    if(x.className === "topnav"){
+      x.className += " responsive";
+    }else{
+      x.className = "topnav";
+    }
   }
-
-  // this logic can be chandes later
-}
-function emptyFields(title, url, text, info) {
-  document.getElementById("title").value = "";
-  document.getElementById("imgsrc").value = "";
-  document.getElementById("text").value = "";
-  setTimeout(function() {
-    document.getElementById("info").innerText = "";
-  }, 4000);
-}
-function showArticles() {
-  for (var i = 0; i < allArticles.length; i++) {
-    let obj = allArticles[i];
-    let objTitle = document.createElement("h3");
-    let titleText = document.createTextNode(obj.title);
-    objTitle.appendChild(titleText);
-
-    let objImage = document.createElement("h3");
-    let imageText = document.createTextNode(obj.image);
-    objImage.appendChild(imageText);
-
-    let objArticle = document.createElement("article");
-    let articleText = document.createTextNode(obj.article);
-    objArticle.appendChild(articleText);
-
-    let objContainer = document.createElement("div");
-    objContainer.appendChild(objTitle);
-    objContainer.appendChild(objArticle);
-    objContainer.appendChild(objImage);
-
-    document.getElementById("articleContainer").appendChild(objContainer);
-  }
-}
+  
